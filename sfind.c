0ce85af094eb273c166f1348c574acf5321cc5dc
davidrsfalcao <davidrsfalcao@gmail.com>
2017-04-13 11:52:27 +0100

    update
    - erro no fork (?)
    - faz print muitas vezes
    - não termina

--- sfind.c
+++ sfind.c
@@ -23,6 +23,101 @@ struct Flags{
     int delete;
 } flags;
 
+void sigint_handler(int signo){
+
+  char terminate;
+
+  kill(getppid(),SIGSTOP);
+
+  printf("\n«Are you sure you want to terminate (Y/N)?»\n");
+  scanf("%s",&terminate);
+
+  terminate = (char)toupper(terminate);
+
+  if(terminate == 'N'){
+    kill(getppid(),SIGCONT);
+  }
+  else{
+    kill(getppid(),SIGQUIT);
+    kill(getpid(),SIGQUIT);
+  }
+
+}
+
+void subscribe_SIGINT(){
+
+  struct sigaction act;
+  act.sa_handler = sigint_handler;
+  act.sa_flags = 0;
+  sigemptyset(&act.sa_mask);
+
+  sigaction(SIGINT,&act,NULL);
+
+}
+
+void searchDir(char* path){
+
+    struct stat fileStatus;
+    struct dirent *fileEntry;
+    DIR* dp;
+    pid_t pid;
+
+    subscribe_SIGINT();
+
+    if (stat(path,&fileStatus) != 0) printf("Stat error\n");
+
+    if (S_ISDIR(fileStatus.st_mode)){
+
+        if ((dp = opendir(path)) == NULL) printf("Error opendir\n");
+
+        char newPath[256];
+
+        while ((fileEntry = readdir(dp)) != NULL){
+
+            //Ignore the current directory and previous directory folders.
+            if (strcmp(fileEntry->d_name, ".") == 0 || strcmp(fileEntry->d_name, "..") == 0){
+                continue;
+            }
+            //Update the current path.
+            strcpy(newPath, path);
+            strcat(newPath, "/");
+            strcat(newPath, fileEntry->d_name);
+            stat(newPath, &fileStatus);
+
+            //Whether the read structure is a file.
+            if (S_ISREG(fileStatus.st_mode)){
+                if (flags.print == 1 && ((strcmp(flags.type,"f")==0) ||
+                 strcmp(flags.name, fileEntry->d_name) == 0
+                 || flags.mode == fileStatus.st_mode)){
+                    printf("%s\n", newPath);
+                }
+            }
+            //Whether the read structure is a directory.
+            if (S_ISDIR(fileStatus.st_mode)){
+                if (flags.print == 1 && (strcmp(flags.type,"d")==0)){
+                    printf("%s\n", newPath);
+                }
+                switch (pid = fork()){
+                    case -1:
+                    //perror("Error fork()...\n");
+                    break;
+
+                    case 0: //child process
+                    searchDir(newPath);
+                    break;
+
+                    default:
+                    waitpid(pid,NULL,WNOHANG);
+                    break;
+
+
+                }
+            }
+        }
+        closedir(dp);
+    }
+}
+
 void printUsage(){
     printf("usage:\n");
     printf("  sfind (starting directory) (matching criteria and actions)\n");
@@ -36,11 +131,17 @@ void printUsage(){
 
 int procArgs(int argc, char *argv[]){
 
+      flags.name = "";
+      flags.type = "$";
+      flags.mode = 022;
+      flags.print = 0;
+      flags.delete = 0;
+
+
     if (argc < 3){
         return -1;
     }
     else for(size_t i = 2; i < argc; i++) {
-
         if(strcmp(argv[i],"-name") == 0) {
             flags.name = argv[i+1];
             i++;
@@ -93,6 +194,8 @@ int main(int argc, char *argv[]){
         {
             //Run the program
             printf("Sucesso ao ler os %d argumentos\n", argc);
+    
+            searchDir(path);
 
             return 0;
         }
