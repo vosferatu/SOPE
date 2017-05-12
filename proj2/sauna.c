d1473e252776b0966edcdeeadc1794a28fd9ed63
davidrsfalcao <davidrsfalcao@gmail.com>
2017-05-12 22:13:07 +0100

    update

--- proj2/sauna.c
+++ proj2/sauna.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define EXIT_ERROR -1
+
+int capacity, vacant_places;
 
 int main(int argc, char* argv[])
 {
@@ -9,4 +12,9 @@ int main(int argc, char* argv[])
         printf("Wrong number of arguments! USAGE: sauna <n. max users>\n\n");
         exit(EXIT_ERROR);
     }
+
+    capacity = atoi(argv[1]);
+    vacant_places = capacity;
+
+    return 0;
 }
