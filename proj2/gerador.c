fd251f6b40be23eee2cff1fa00ceb57e48a46e45
davidrsfalcao <davidrsfalcao@gmail.com>
2017-05-12 21:43:48 +0100

    update

--- proj2/gerador.c
+++ proj2/gerador.c
@@ -1,4 +1,4 @@
-int main(int num, char * path[])
+int main(int argc, char* argv[])
 {
 
 }
--- proj2/sauna.c
+++ proj2/sauna.c
@@ -1,4 +1,12 @@
-int main(int num, char * path[])
-{
+#include <stdlib.h>
+#include <stdio.h>
 
+#define EXIT_ERROR -1
+
+int main(int argc, char* argv[])
+{
+    if(argc != 2){
+        printf("Wrong number of arguments! USAGE: sauna <n. max users>\n\n");
+        exit(EXIT_ERROR);
+    }
 }
