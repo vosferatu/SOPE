615c8d48f6bf64baa73c3b2926b07945e7d0dbc4
Nuno Pereira <up201506265@fe.up.pt>
2017-05-14 23:26:14 +0100

    Updated

--- proj2/gerador.c
+++ proj2/gerador.c
@@ -5,34 +5,49 @@
 #define BUF_LENGTH 256
 
 	typedef struct pedido{
+		int id;
 		char gender;
 		int d;
 	}Pedido;
 
-void gerador(int nPedidos, int maxUtilizacao){
+		char *getGender(){
+			char gender = '';
+			int r = rand() % 2;
+			if(r==0)
+				gender = 'M';
+			else gender = 'F';
+			return gender;
+		}
+
+		int *getRandomTime(){
+			int d=0;
+			d = rand() % maxUtilizacao + 1;
+		}
+
+		void escrevePedidoEmFicheiro(Pedido ped){
+			FILE * dst= fopen("output", "w");
+			if (dst != NULL) {
+				fwrite(ped, sizeof(struct pedido), 1, dst);
+				fclose(file);
+			}
+		}
+
+
+void gerarPedidos(int nPedidos, int maxUtilizacao){
 	int pedidos[nPedidos];
-	int counter = 0;
-	char gender = '';
-	int d=0;
+	int counter = 1
 	char buf[BUF_LENGTH];
 
 	while(counter < nPedidos){
-		int r = rand() % 3;
-		if(r==1)
-			gender = 'M';
-		else gender = 'F';
 
-		d = rand() % (maxUtilizacao+1);
 		Pedido *p = malloc(sizeof(struct pedido));
+		p->id = counter;
 		p->gender = gender;
 		p->d = d;
 
+		escrevePedidoEmFicheiro(p);
 
-		FILE * dst= fopen("output", "w");
-		if (dst != NULL) {
-		    fwrite(p, sizeof(struct pedido), 1, dst);
-		    fclose(file);
-		}
+		counter++;
 	}
 
 
@@ -60,4 +75,3 @@ void gerador(int nPedidos, int maxUtilizacao){
 
 		 ENTRADA_FIFO_FD = open("/tmp/entrada", O_WRONLY | O_NONBLOCK);
 }
-
